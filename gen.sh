2a5ae62fb2e614e168cafb2d1ea93884978e41eb
burton <burton@spinn3r.com>
2018-09-25 08:43:16 -0700

    all files should be ok

--- gen.sh
+++ gen.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# mustache sucks.
+run_template() {
+    name=${1}
 
-mustache -p partials/head.mustache \
-         -p partials/header.mustache \
-         -p partials/footer.mustache terms-of-service.json terms-of-service.mustache > terms-of-service.html
+    # mustache sucks.
+
+    echo "{}" > /tmp/${name}.json
+
+    mustache -p partials/head.mustache \
+             -p partials/header.mustache \
+             -p partials/footer.mustache \
+             /tmp/${name}.json ${name}.mustache > ${name}.html
+
+}
+
+run_template terms-of-service
+run_template privacy-policy
+run_template terms-of-service
