5168d4733a5995f4e3c61ab7bd19d6fabb09504f
burton <burton@spinn3r.com>
2018-10-08 11:56:55 -0700

    downloads page should be ready..

--- download.js
+++ download.js
@@ -1,13 +1,19 @@
 const downloads = {
 
     "download-win-64": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-x64.exe",
-    "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-x32.exe",
-    "download-mac-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210.dmg",
+    "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-ia32.exe",
+    "download-macos-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210.dmg",
     "download-linux-deb": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-amd64.deb",
     "download-linux-targz": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-x64.tar.gz",
 
 };
 
 function configureDownloads() {
+    for (const download of Object.keys(downloads)) {
+        var href = downloads[download];
+        document.getElementById(download).setAttribute("href", href);
+    }
 
 }
+
+configureDownloads();
