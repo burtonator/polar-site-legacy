e0d7cab370e6bebf7ab84ac68526fd22c5a6fca6
burton <burton@spinn3r.com>
2018-10-08 11:32:25 -0700

    mac links...

---
+++ download.js
@@ -0,0 +1,13 @@
+const downloads = {
+
+    "download-win-64": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-x64.exe",
+    "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-x32.exe",
+    "download-mac-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210.dmg",
+    "download-linux-deb": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-amd64.deb",
+    "download-linux-targz": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.0-beta210/polar-bookshelf-1.0.0-beta210-x64.tar.gz",
+
+};
+
+function configureDownloads() {
+
+}
