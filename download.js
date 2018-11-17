19440edd28889e9d07832f048032fed9e3bf0db9
burton <burton@spinn3r.com>
2018-11-17 13:03:27 -0800

    version bump...

--- download.js
+++ download.js
@@ -1,11 +1,11 @@
 const downloads = {
 
-    "download-win-64": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.13/polar-bookshelf-1.0.13-x64.exe",
-    "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.13/polar-bookshelf-1.0.13-ia32.exe",
-    "download-macos-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.13/polar-bookshelf-1.0.13.dmg",
-    "download-linux-deb": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.13/polar-bookshelf-1.0.13-amd64.deb",
-    "download-linux-targz": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.13/polar-bookshelf-1.0.13-x64.tar.gz",
-    "download-linux-appimage": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.13/polar-bookshelf-1.0.13-x86_64.AppImage",
+    "download-win-64": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.14/polar-bookshelf-1.0.14-x64.exe",
+    "download-win-32": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.14/polar-bookshelf-1.0.14-ia32.exe",
+    "download-macos-dmg": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.14/polar-bookshelf-1.0.14.dmg",
+    "download-linux-deb": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.14/polar-bookshelf-1.0.14-amd64.deb",
+    "download-linux-targz": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.14/polar-bookshelf-1.0.14-x64.tar.gz",
+    "download-linux-appimage": "https://github.com/burtonator/polar-bookshelf/releases/download/v1.0.14/polar-bookshelf-1.0.14-x86_64.AppImage",
 
 };
 
